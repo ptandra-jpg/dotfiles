db653c85fa4d6869d7ceafeb27dbe5cfbdfaaf70
Pradyun Tandra <ptandra@coreweave.com>
2026-06-03 09:46:14 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Install zsh if not present
+if ! command -v zsh &> /dev/null; then
+  sudo apt-get install -y zsh
+fi
+
+# Install starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
+
+# Symlink dotfiles
+DOTFILES="$HOME/dotfiles"
+ln -sf "$DOTFILES/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES/.zprofile" "$HOME/.zprofile"
+ln -sf "$DOTFILES/.gitconfig" "$HOME/.gitconfig"
+mkdir -p "$HOME/.config"
+ln -sf "$DOTFILES/starship.toml" "$HOME/.config/starship.toml"
+mkdir -p "$HOME/.ssh"
+ln -sf "$DOTFILES/config" "$HOME/.ssh/config"
+
+# Set zsh as default shell
+chsh -s $(which zsh)
